30d24be4f3685c0c585ba4f19a9ddb8e58fc3874
sailesh <vermasailesh271@gmail.com>
2023-12-28 15:06:45 +0530

    added required state provider

---
+++ lib/providers/providers.dart
@@ -0,0 +1,18 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:timezy/utils/app_config.dart';
+import 'package:timezy/utils/random_number_geenrator.dart';
+
+final remainingTimeStateProvider =
+    StateProvider((ref) => AppConfig.timeForClick);
+
+final currentTimeInSeconds = StateProvider((ref) => DateTime.now().second);
+
+final randomNumberGeneratorStateProvider =
+    StateProvider((ref) => randomNumberGenerator());
+
+final attemptRemainingStateProvider =
+    StateProvider((ref) => AppConfig.totalAttempt);
+
+final currentScoreStateProvider = StateProvider((ref) => 0);
+
+final showWinCardProvider = StateProvider((ref) => false);
